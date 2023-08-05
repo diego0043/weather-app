0a52b31bfe69c44f2597f5c271c39a696a1413b0
Diego Valencia <ve18007@ues.edu.sv>
2023-08-04 19:51:04 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <>
+      <div>
+        <button className="btn btn-outline-primary ms-5">Click me</button>
+      </div>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {App} from './App.jsx'
+import './index.css'
+import css_styles from 'bootstrap/dist/css/bootstrap.min.css'
+import js_utilities from 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
